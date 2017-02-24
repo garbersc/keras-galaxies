6e974dddfaa8c99e2a722544efc7379f8e488955
Christoph Garbers <chgristoph.garbers@desy.de>
2017-02-24 12:08:35 +0100

    running on WinSol weights without dropouts

--- custom_for_keras.py
+++ custom_for_keras.py
@@ -22,7 +22,7 @@ def kaggle_MultiRotMergeLayer_output_shape(input_shape , num_views=2 ):
 
 def kaggle_input(x, part_size = 45, n_input_var = 2, include_flip=False, random_flip=False ):
 	parts = []
-	for i in range(0,n_input_var):
+	for i in range(n_input_var/2 if include_flip else n_input_var):
 	    input_=x[i]
             ps = part_size # shortcut 
 
@@ -54,110 +54,99 @@ def kaggle_sliced_accuracy(y_true, y_pred):
 		
 		
 	
-def dense_weight_init_values( n_inputs , n_outputs , w_std = 0.001 , b_init_val = 0.01 ):
+def dense_weight_init_values( n_inputs , n_outputs, nb_feature = None , w_std = 0.001 , b_init_val = 0.01 ):
 	if type(n_inputs)!=tuple and type(n_inputs)==int: n_inputs = (n_inputs,)
 	if type(n_outputs)!=tuple and type(n_outputs)==int: n_outputs = (n_outputs,)
 	W_shape = n_inputs + n_outputs
-	return ( np.random.randn(*W_shape).astype(np.float32)*w_std , np.ones(*n_outputs).astype(np.float32)*b_init_val )
+	if nb_feature:
+		W_shape = (nb_feature,) + W_shape
+		n_outputs = (nb_feature,) + n_outputs
+	return ( np.random.randn(*W_shape).astype(np.float32)*w_std , np.ones(n_outputs).astype(np.float32)*b_init_val )
 
 
 
 def OptimisedDivGalaxyOutput(x,mb_size ,normalised=True, categorised=False): #TODO categorised and LL loss not updated
-        input_layer = x
-        params = []
-        bias_params = []
+
+
+	input_layer = x
+	params = []
+	bias_params = []
         
-        question_slices = [slice(0, 3), slice(3, 5), slice(5, 7), slice(7, 9), slice(9, 13), slice(13, 15),
+	question_slices = [slice(0, 3), slice(3, 5), slice(5, 7), slice(7, 9), slice(9, 13), slice(13, 15),
                                 slice(15, 18), slice(18, 25), slice(25, 28), slice(28, 31), slice(31, 37)]
 
 
-        normalisation_mask = T.variable(generate_normalisation_mask(question_slices))
+	normalisation_mask = T.variable(generate_normalisation_mask(question_slices))
         # self.scaling_mask = theano.shared(self.generate_scaling_mask())
 
         # sequence of scaling steps to be undertaken.
         # First element is a slice indicating the values to be scaled. Second element is an index indicating the scale factor.
         # these have to happen IN ORDER else it doesn't work correctly.
-        scaling_sequence = [
+	scaling_sequence = [
             (slice(3, 5), 1), # I: rescale Q2 by A1.2
             (slice(5, 13), 4), # II: rescale Q3, Q4, Q5 by A2.2
             (slice(15, 18), 0), # III: rescale Q7 by A1.1
             (slice(18, 25), 13), # IV: rescale Q8 by A6.1
             (slice(25, 28), 3), # V: rescale Q9 by A2.1
             (slice(28, 37), 7), # VI: rescale Q10, Q11 by A4.1
-        ]
+	]
 
 	if normalised: return predictions(input_layer,normalisation_mask,categorised,mb_size,scaling_sequence)
 	else: return predictions_no_normalisation(input_layer)
 
 def predictions(input_layer,normalisation_mask,categorised,mb_size,scaling_sequence, *args, **kwargs):
-        return weighted_answer_probabilities(input_layer,normalisation_mask,categorised,mb_size,scaling_sequence, *args, **kwargs)
+	        return weighted_answer_probabilities(input_layer,normalisation_mask,categorised,mb_size,scaling_sequence, *args, **kwargs)
 
 def predictions_no_normalisation(input_layer,*args, **kwargs):
-        """
-        Predict without normalisation. This can be used for the first few chunks to find good parameters.
-        """
-        return T.clip(input_layer, 0, 1) # clip on both sides here, any predictions over 1.0 are going to get normalised away anyway.
+        	"""
+       		Predict without normalisation. This can be used for the first few chunks to find good parameters.
+        	"""
+       		return T.clip(input_layer, 0, 1) # clip on both sides here, any predictions over 1.0 are going to get normalised away anyway.
 
 
 
 def generate_normalisation_mask(question_slices):
-        """
-        when the clipped input is multiplied by the normalisation mask, the normalisation denominators are generated.
-        So then we can just divide the input by the normalisation constants (elementwise).
-        """
-        mask = np.zeros((37, 37), dtype='float32')
-        for s in question_slices:
-            mask[s, s] = 1.0
-        return mask
-'''
-def error(self, normalisation=True, *args, **kwargs):
-        if normalisation:
-            predictions = self.predictions(*args, **kwargs)
-        else:
-            predictions = self.predictions_no_normalisation(*args, **kwargs)
-        error = T.mean((predictions - self.target_var) ** 2)
-        return error
-'''
-    # def generate_scaling_mask(self):
-    #     """
-    #     This mask needs to be applied to the LOGARITHM of the probabilities. The appropriate log probs are then summed,
-    #     which corresponds to multiplying the raw probabilities, which is what we want to achieve.
-    #     """
-    #     mask = np.zeros((37, 37), dtype='float32')
-    #     for s, factor_indices in zip(self.question_slices, self.scaling_factor_indices):
-    #         if factor_indices is not None:
-    #             mask[factor_indices, s] = 1.0
-    #     return mask
+        	"""
+        	when the clipped input is multiplied by the normalisation mask, the normalisation denominators are generated.
+        	So then we can just divide the input by the normalisation constants (elementwise).
+        	"""
+        	mask = np.zeros((37, 37), dtype='float32')
+        	for s in question_slices:
+            		mask[s, s] = 1.0
+        	return mask
+
 
 def weighted_answer_probabilities(input_layer,normalisation_mask,categorised,mb_size,scaling_sequence, *args, **kwargs):
-        probs = answer_probabilities(input_layer,normalisation_mask,categorised,mb_size,*args, **kwargs)
-        # go through the rescaling sequence in order (6 steps)
-	output = []
-	output.append(probs[:,0:3])
-	if not categorised:
+        	probs = answer_probabilities(input_layer,normalisation_mask,categorised,mb_size,*args, **kwargs)
+        	# go through the rescaling sequence in order (6 steps)
+		output = []
+		output.append(probs[:,0:3])
+		if not categorised:
 		#prob_scale = T.ones(T.shape(probs))
-        	for probs_slice, scale_idx in scaling_sequence:				
-         	   #probs = T.set_subtensor(probs[:, probs_slice], probs[:, probs_slice] * probs[:, scale_idx].dimshuffle(0, 'x'))
-			output.append( probs[:, probs_slice] * probs[:, scale_idx].dimshuffle(0, 'x') )
-			if probs_slice==slice(5, 13): output.append(probs[:,13:15])
-			#T.batch_set_value(probs[:, probs_slice], probs[:, probs_slice] * probs[:, scale_idx].dimshuffle(0, 'x'))
-		output = T.concatenate(output)
-	else: output = probs
+        		for probs_slice, scale_idx in scaling_sequence:				
+        	 	   #probs = T.set_subtensor(probs[:, probs_slice], probs[:, probs_slice] * probs[:, scale_idx].dimshuffle(0, 'x'))
+				output.append( probs[:, probs_slice] * probs[:, scale_idx].dimshuffle(0, 'x') )
+				if probs_slice==slice(5, 13): output.append(probs[:,13:15])
+				#T.batch_set_value(probs[:, probs_slice], probs[:, probs_slice] * probs[:, scale_idx].dimshuffle(0, 'x'))
+			output = T.concatenate(output)
+		else: output = probs
 
-        return output
+        	return output
 
 def answer_probabilities(x,normalisation_mask,categorised,mb_size, *args, **kwargs):
-        """
-        normalise the answer groups for each question.
-        """
-        input_ = T.reshape(x,(mb_size,37))
-        input_clipped = T.maximum(input_, 0) # T.clip(input, 0, 1) # T.maximum(input, 0)	
+        	"""
+        	normalise the answer groups for each question.
+        	"""
+        	input_ = T.reshape(x,(mb_size,37))
+        	input_clipped = T.maximum(input_, 0) # T.clip(input, 0, 1) # T.maximum(input, 0)	
 
-        normalisation_denoms = T.dot(input_clipped, normalisation_mask) + 1e-7 # small constant to prevent division by 0
-        input_normalised = input_clipped / normalisation_denoms
+        	normalisation_denoms = T.dot(input_clipped, normalisation_mask) + 1e-7 # small constant to prevent division by 0
+        	input_normalised = input_clipped / normalisation_denoms
 
-	'''
-	if categorised:
+        	return input_normalised
+
+		'''
+		if categorised:
 		output=[]
 		for k in xrange(0,mb_size):
 		    input_q = [
@@ -200,11 +189,11 @@ def answer_probabilities(x,normalisation_mask,categorised,mb_size, *args, **kwar
     		#input_normalised[18:25]=input_q[7]
     		#input_normalised[25:28]=input_q[8]
     		#input_normalised[28:31]=input_q[9]
-    		#input_normalised[31:37]=input_q[10]
-	'''	
-        return input_normalised
-        # return [input_normalised[:, s] for s in self.question_slices]
+    		#input_normalised[31:37]=input_q[10]	
+        	return input_normalised
+        	# return [input_normalised[:, s] for s in self.question_slices]
 
+		'''
 
 
 
--- try_convnet_keras_customConv_noTVar.py
+++ try_convnet_keras_customConv_noTVar.py
@@ -1,5 +1,6 @@
 import theano.sandbox.cuda.basic_ops as sbcuda
 import numpy as np
+# import pandas as pd
 import keras.backend as T
 import load_data
 import realtime_augmentation as ra
@@ -10,7 +11,7 @@ import cPickle as pickle
 from datetime import datetime, timedelta
 
 from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, MaxPooling1D, Dropout, Input, Convolution2D, MaxoutDense
+from keras.layers import Dense, Activation, MaxPooling1D, Dropout, Input, Convolution2D
 from keras.layers.core import Lambda, Flatten, Reshape, Permute
 from keras.optimizers import SGD, Adam
 from keras.engine.topology import Merge
@@ -21,7 +22,12 @@ import functools
 from keras_extra_layers import kerasCudaConvnetPooling2DLayer, kerasCudaConvnetConv2DLayer, fPermute
 from custom_for_keras import kaggle_MultiRotMergeLayer_output, OptimisedDivGalaxyOutput, kaggle_input, kaggle_sliced_accuracy, dense_weight_init_values
 
+# import matplotlib.pyplot as plt 
+# plt.ion()
+# import utils
+
 debug = True
+predict = False
 
 continueAnalysis = False
 saveAtEveryLearningRate = True
@@ -49,7 +55,7 @@ ra.y_train=y_train
 # split training data into training + a small validation set
 ra.num_train = y_train.shape[0]
 
-ra.num_valid = ra.num_train // 10 # integer division, is defining validation size
+ra.num_valid = ra.num_train // 20 # integer division, is defining validation size
 ra.num_train -= ra.num_valid
 
 ra.y_valid = ra.y_train[ra.num_train:]
@@ -87,16 +93,25 @@ test_indices = np.arange(num_test)
 BATCH_SIZE = 16
 NUM_INPUT_FEATURES = 3
 
-LEARNING_RATE_SCHEDULE = {
+LEARNING_RATE_SCHEDULE = {  #if adam is used the learning rate doesnt follow the schedule
     0: 0.04,
-    1800: 0.004,
-    2300: 0.0004,
+    200: 0.05,
+    400: 0.001,
+    800: 0.0005
+    #500: 0.04,
+    #0: 0.01,
+    #1800: 0.004,
+    #2300: 0.0004,
+   # 0: 0.08,
+   # 50: 0.04,
+   # 2000: 0.008,
+   # 3200: 0.0008,
+   # 4600: 0.0004,
 }
-
 if continueAnalysis or getWinSolWeights :
     LEARNING_RATE_SCHEDULE = {
         0: 0.01,	
-        500: 0.005,
+        500: 0.002,
         #800: 0.0004,
         3200: 0.0002,
         4600: 0.0001,
@@ -105,9 +120,9 @@ if continueAnalysis or getWinSolWeights :
 
 MOMENTUM = 0.9
 WEIGHT_DECAY = 0.0
-CHUNK_SIZE = 10000#1008#10000 # 30000 # this should be a multiple of the batch size, ideally.
-NUM_CHUNKS = 1000#1000#7000 #2500 # 3000 # 1500 # 600 # 600 # 600 # 500   
-VALIDATE_EVERY = 10 #20 # 12 # 6 # 6 # 6 # 5 # validate only every 5 chunks. MUST BE A DIVISOR OF NUM_CHUNKS!!!
+CHUNK_SIZE = 1008#1008#10000 # 30000 # this should be a multiple of the batch size, ideally.
+NUM_CHUNKS = 5#1000#7000 #2500 # 3000 # 1500 # 600 # 600 # 600 # 500   
+VALIDATE_EVERY = 5 #20 # 12 # 6 # 6 # 6 # 5 # validate only every 5 chunks. MUST BE A DIVISOR OF NUM_CHUNKS!!!
 # else computing the analysis data does not work correctly, since it assumes that the validation set is still loaded.
 print("The training is running for %s chunks, each with %s images. That are about %s epochs. The validation sample contains %s images. \n" % (NUM_CHUNKS,CHUNK_SIZE,CHUNK_SIZE*NUM_CHUNKS / (ra.num_train-CHUNK_SIZE) ,  ra.num_valid ))
 NUM_CHUNKS_NONORM =  1 # train without normalisation for this many chunks, to get the weights in the right 'zone'.
@@ -139,10 +154,10 @@ WEIGHTS=WEIGHTS/WEIGHTS[WEIGHTS.argmax()]
 
 GEN_BUFFER_SIZE = 1
 
-TRAIN_LOSS_SF_PATH = "trainingNmbrs_keras_maxout_start_w_winsolWeights_noDropout.txt"
+TRAIN_LOSS_SF_PATH = "trainingNmbrs_keras_start_w_winsolWeights.txt"
 
-ANALYSIS_PATH = "analysis/final/try_convent_keras_maxout_start_w_winsolWeights_noDropout.pkl"
-WEIGHTS_PATH = "analysis/final/try_convent_keras_maxout_start_w_winsolWeights_noDropout.h5"
+#TARGET_PATH = "predictions/final/try_convnet.csv"
+ANALYSIS_PATH = "analysis/final/try_convent_keras_start_w_winsolWeights.pkl"
 
 with open(TRAIN_LOSS_SF_PATH, 'a')as f:
 	if continueAnalysis: 
@@ -254,18 +269,18 @@ model.add(fPermute((1,2,3,0)))
 
 if debug : print model.output_shape
 
-model.add(kerasCudaConvnetConv2DLayer(n_filters=32, filter_size=6, untie_biases=True)) 
+model.add(kerasCudaConvnetConv2DLayer(n_filters=32, filter_size=6 , untie_biases=True)) 
 if debug : print model.output_shape
 model.add(kerasCudaConvnetPooling2DLayer())
 
 if debug : print model.output_shape
 
-model.add(kerasCudaConvnetConv2DLayer(n_filters=64, filter_size=5, untie_biases=True)) 
+model.add(kerasCudaConvnetConv2DLayer(n_filters=64, filter_size=5 , untie_biases=True)) 
 if debug : print model.output_shape
 model.add(kerasCudaConvnetPooling2DLayer())
 
-model.add(kerasCudaConvnetConv2DLayer(n_filters=128, filter_size=3, untie_biases=True)) 
-model.add(kerasCudaConvnetConv2DLayer(n_filters=128, filter_size=3,  weights_std=0.1 , untie_biases=True)) 
+model.add(kerasCudaConvnetConv2DLayer(n_filters=128, filter_size=3 , untie_biases=True)) 
+model.add(kerasCudaConvnetConv2DLayer(n_filters=128, filter_size=3,  weights_std=0.1 , untie_biases=True )) 
 
 if debug : print model.output_shape
 
@@ -281,16 +296,25 @@ model.add(Lambda(function=kaggle_MultiRotMergeLayer_output, output_shape=lambda 
 
 if debug : print model.output_shape
 
-model.add(MaxoutDense(output_dim=2048, nb_feature=2 ,weights = dense_weight_init_values(model.output_shape[-1],2048,nb_feature=2) )) 
-#model.add(Dropout(0.5))
-
-model.add(MaxoutDense(output_dim=2048, nb_feature=2 ,weights = dense_weight_init_values(model.output_shape[-1],2048,nb_feature=2) )) 
-#model.add(Dropout(0.5))
+#model.add(Dense(output_dim=4096, init=functools.partial(initializations.normal, scale=0.001) )) 
+model.add(Dense(output_dim=4096, weights = dense_weight_init_values(model.output_shape[-1],4096) )) 
+model.add(Dropout(0.5))
+model.add(Reshape((4096,1))) 
+model.add(MaxPooling1D())
+model.add(Reshape((2048,)))
+model.add(Dense(output_dim=4096, weights = dense_weight_init_values(model.output_shape[-1],4096) ))
 
 if debug : print model.output_shape
 
+model.add(Dropout(0.5))
+model.add(Reshape((4096,1)))
+model.add(MaxPooling1D())
+
+if debug : print model.output_shape
+
+model.add(Reshape((2048,)))
 model.add(Dense(output_dim=37, weights = dense_weight_init_values(model.output_shape[-1],37 ,w_std = 0.01 , b_init_val = 0.1 ) ))
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 
 if debug : print model.output_shape
 
@@ -317,8 +341,6 @@ def lr_function(e):
 	return _current_lr
 lr_callback = LearningRateScheduler(lr_function)
 
-maxdense_layers_ids = [4,5]
-
 if getWinSolWeights:
 	w_load_worked = False
 	for l in model_norm.layers:
@@ -326,12 +348,6 @@ if getWinSolWeights:
 		if debug: print len(l.get_weights())
 		l_weights = l.get_weights()
 		if len(l_weights)==len(w_kSorted):
-
-			for i in maxdense_layers_ids:
-				if debug: print('--- ',i, np.shape(w_kSorted[2*i]))
-				w_kSorted[2*i] = w_kSorted[2*i].reshape( ( 2 , np.shape(w_kSorted[2*i])[0] , np.shape(w_kSorted[2*i])[1]/2 ) )		
-				w_kSorted[2*i+1] = w_kSorted[2*i+1].reshape( ( 2 , np.shape(w_kSorted[2*i+1])[0]/2 ) )	
-
 			if debug:
 				for i in range(len(l_weights)):
     					print type(l_weights[i])
@@ -349,12 +365,15 @@ model_noNorm.compile(loss='mean_squared_error', optimizer=SGD(lr=LEARNING_RATE_S
 #model_norm.compile(loss='mean_squared_error', optimizer=adam, metrics=['categorical_accuracy',kaggle_sliced_accuracy])
 #model_noNorm.compile(loss='mean_squared_error', optimizer=adam, metrics=['categorical_accuracy'])
 
+#xs_shared = [T.variable(np.zeros((1,1,1,1), dtype='float32')) for _ in xrange(num_input_representations)] 
+#y_shared = T.variable(np.zeros((1,1), dtype='float32'))
+
 xs_shared = [np.zeros((1,1,1,1), dtype='float32') for _ in xrange(num_input_representations)] 
 y_shared = np.zeros((1,1), dtype='float32')
 
-if continueAnalysis:
+if continueAnalysis:#TODO
     print "Load model weights"
-    model_norm.load_weights(WEIGHTS_PATH)
+    model_norm.load_weights(filepath)
 
 print "Train model"
 start_time = time.time()
@@ -379,7 +398,7 @@ for e in xrange(NUM_CHUNKS):
     xs_chunk = [x_chunk.transpose(0, 3, 1, 2) for x_chunk in xs_chunk]
 
     # train without normalisation for the first # chunks.
-    if e >= NUM_CHUNKS_NONORM or continueAnalysis:
+    if e >= NUM_CHUNKS_NONORM or continueAnalysis or getWinSolWeights:
         model_inUse = model_norm
         #if  USE_LLERROR:  train = train_norm_ll
     else:
@@ -388,10 +407,10 @@ for e in xrange(NUM_CHUNKS):
 
     num_batches_chunk = x_chunk.shape[0] // BATCH_SIZE
 
-    #update learning rate, use chunks on epoch function 
-    lr_callback.model = model_inUse
-    lr_callback.on_epoch_begin(e)
-    if e in LEARNING_RATE_SCHEDULE: print "  setting learning rate to %.6f" % current_lr
+    #update learning rate, use chunks on epoch function #FIXME reanable lr schedule
+    #lr_callback.model = model_inUse
+    #lr_callback.on_epoch_begin(e)
+    #if e in LEARNING_RATE_SCHEDULE: print "  setting learning rate to %.6f" % current_lr
 
     print "  batch SGD"
     losses = []
@@ -404,8 +423,8 @@ for e in xrange(NUM_CHUNKS):
     	l0_45_input_var= xs_chunk[1][b*BATCH_SIZE:(b+1)*BATCH_SIZE]
 	l6_target_var=  y_chunk[b*BATCH_SIZE:(b+1)*BATCH_SIZE]
 	
-	lossaccuracy = model_inUse.train_on_batch( [l0_input_var,l0_45_input_var] , l6_target_var )
 	if ((e + 1) % VALIDATE_EVERY) == 0: loss_test = model_inUse.test_on_batch([l0_input_var,l0_45_input_var], l6_target_var )[0]
+	lossaccuracy = model_inUse.train_on_batch( [l0_input_var,l0_45_input_var] , l6_target_var )
 	loss = lossaccuracy[0]
 
         losses.append(loss)
@@ -435,6 +454,8 @@ for e in xrange(NUM_CHUNKS):
 	losses_ll = []
 	losses_weighted = []
         for b in xrange(num_batches_valid):
+            # if b % 1000 == 0:
+            #     print "  batch %d/%d" % (b + 1, num_batches_valid)
 
 	    l0_input_var= xs_valid[0][b*BATCH_SIZE:(b+1)*BATCH_SIZE]
     	    l0_45_input_var= xs_valid[1][b*BATCH_SIZE:(b+1)*BATCH_SIZE]
@@ -469,6 +490,9 @@ for e in xrange(NUM_CHUNKS):
         mean_sliced_accuracy = np.mean(sliced_accuracies)
         mean_sliced_accuracy_std = np.mean(sliced_accuracies_std)
     	#mean_valid_loss_ll = np.mean(losses_ll)
+
+
+        #if USE_WEIGHTS:  mean_valid_loss_weighted = np.sqrt(np.mean(losses_weighted))
 
         print "  mean validation loss (RMSE):\t\t%.6f" % mean_valid_loss
         print "  mean validation accuracy:\t\t%.6f" % mean_accuracy
@@ -519,7 +543,8 @@ for e in xrange(NUM_CHUNKS):
         			'losses_train': losses_train,
         			'losses_valid': losses_valid
     				}, f, pickle.HIGHEST_PROTOCOL)
-		model_inUse.save_weights(WEIGHTS_PATH)
+		LR_PATH = ((ANALYSIS_PATH.split('.',1)[0]+'toLearningRate%s.h5')%current_lr)
+		model_inUse.save_weights(LR_PATH)
 		
 	with open(TRAIN_LOSS_SF_PATH, 'a')as f:
 	   f.write("#  setting learning rate to %.6f \n" % current_lr )
@@ -531,6 +556,8 @@ print "Compute predictions on validation set for analysis in batches"
 predictions_list = []
 
 for b in xrange(num_batches_valid):
+    # if b % 1000 == 0:
+    #     print "  batch %d/%d" % (b + 1, num_batches_valid)
 
     l0_input_var= xs_valid[0][b*BATCH_SIZE:(b+1)*BATCH_SIZE]
     l0_45_input_var= xs_valid[1][b*BATCH_SIZE:(b+1)*BATCH_SIZE]
@@ -557,12 +584,107 @@ with open(ANALYSIS_PATH, 'w') as f:
         'losses_train': losses_train,
         'losses_valid': losses_valid
     }, f, pickle.HIGHEST_PROTOCOL)
-model_norm.save_weights(WEIGHTS_PATH)
+LR_PATH = (ANALYSIS_PATH.split('.',1)[0]+'.h5')
+model_norm.save_weights(LR_PATH)
 
 
 del chunk_data, xs_chunk, x_chunk, y_chunk, xs_valid, x_valid # memory cleanup
 del predictions_list, all_predictions # memory cleanup
 
-print "Done!"
 
-exit()
+
+
+if not predict:
+	exit()
+
+'''
+
+print "Computing predictions on test data"
+predictions_list = []
+for e, (xs_chunk, chunk_length) in enumerate(create_test_gen()):
+    print "Chunk %d" % (e + 1)
+    xs_chunk = [x_chunk.transpose(0, 3, 1, 2) for x_chunk in xs_chunk] # move the colour dimension up.
+
+    for x_shared, x_chunk in zip(xs_shared, xs_chunk):
+        x_shared.set_value(x_chunk)
+
+    num_batches_chunk = int(np.ceil(chunk_length / float(BATCH_SIZE)))  # need to round UP this time to account for all data
+
+    # make predictions for testset, don't forget to cute off the zeros at the end
+    for b in xrange(num_batches_chunk):
+        # if b % 1000 == 0:
+        #     print "  batch %d/%d" % (b + 1, num_batches_chunk)
+
+        predictions = compute_output(b)
+        predictions_list.append(predictions)
+
+
+all_predictions = np.vstack(predictions_list)
+all_predictions = all_predictions[:num_test] # truncate back to the correct length
+
+# postprocessing: clip all predictions to 0-1
+all_predictions[all_predictions > 1] = 1.0
+all_predictions[all_predictions < 0] = 0.0
+
+
+print "Write predictions to %s" % TARGET_PATH
+# test_ids = np.load("data/test_ids.npy")
+
+
+with open(TARGET_PATH, 'wb') as csvfile:
+    writer = csv.writer(csvfile) # , delimiter=',', quoting=csv.QUOTE_MINIMAL)
+
+    # write header
+    writer.writerow(['GalaxyID', 'Class1.1', 'Class1.2', 'Class1.3', 'Class2.1', 'Class2.2', 'Class3.1', 'Class3.2', 'Class4.1', 'Class4.2', 'Class5.1', 'Class5.2', 'Class5.3', 'Class5.4', 'Class6.1', 'Class6.2', 'Class7.1', 'Class7.2', 'Class7.3', 'Class8.1', 'Class8.2', 'Class8.3', 'Class8.4', 'Class8.5', 'Class8.6', 'Class8.7', 'Class9.1', 'Class9.2', 'Class9.3', 'Class10.1', 'Class10.2', 'Class10.3', 'Class11.1', 'Class11.2', 'Class11.3', 'Class11.4', 'Class11.5', 'Class11.6'])
+
+    # write data
+    for k in xrange(test_ids.shape[0]):
+        row = [test_ids[k]] + all_predictions[k].tolist()
+        writer.writerow(row)
+
+print "Gzipping..."
+os.system("gzip -c %s > %s.gz" % (TARGET_PATH, TARGET_PATH))
+
+
+del all_predictions, predictions_list, xs_chunk, x_chunk # memory cleanup
+
+'''
+
+# # need to reload training data because it has been split and shuffled.
+# # don't need to reload test data
+# x_train = load_data.load_gz(DATA_TRAIN_PATH)
+# x2_train = load_data.load_gz(DATA2_TRAIN_PATH)
+# x_train = x_train.transpose(0, 3, 1, 2) # move the colour dimension up
+# x2_train = x2_train.transpose(0, 3, 1, 2)
+# train_gen_features = load_data.array_chunker_gen([x_train, x2_train], chunk_size=CHUNK_SIZE, loop=False, truncate=False, shuffle=False)
+# test_gen_features = load_data.array_chunker_gen([x_test, x2_test], chunk_size=CHUNK_SIZE, loop=False, truncate=False, shuffle=False)
+
+
+# for name, gen, num in zip(['train', 'test'], [train_gen_features, test_gen_features], [x_train.shape[0], x_test.shape[0]]):
+#     print "Extracting feature representations for all galaxies: %s" % name
+#     features_list = []
+#     for e, (xs_chunk, chunk_length) in enumerate(gen):
+#         print "Chunk %d" % (e + 1)
+#         x_chunk, x2_chunk = xs_chunk
+#         x_shared.set_value(x_chunk)
+#         x2_shared.set_value(x2_chunk)
+
+#         num_batches_chunk = int(np.ceil(chunk_length / float(BATCH_SIZE)))  # need to round UP this time to account for all data
+
+#         # compute features for set, don't forget to cute off the zeros at the end
+#         for b in xrange(num_batches_chunk):
+#             if b % 1000 == 0:
+#                 print "  batch %d/%d" % (b + 1, num_batches_chunk)
+
+#             features = compute_features(b)
+#             features_list.append(features)
+
+#     all_features = np.vstack(features_list)
+#     all_features = all_features[:num] # truncate back to the correct length
+
+#     features_path = FEATURES_PATTERN % name 
+#     print "  write features to %s" % features_path
+#     np.save(features_path, all_features)
+
+
+print "Done!"
